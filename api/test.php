194fdc44fdee5a60e07f0ad0c8fb9b86cef42b7d
Dandee Galang <dandeegalang.ph@gmail.com>
2025-08-07 03:14:11 +0800

    Delete api directory

--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simple PHP test
-header('Content-Type: application/json');
-echo json_encode([
-    'message' => 'PHP is working!',
-    'time' => date('Y-m-d H:i:s'),
-    'php_version' => PHP_VERSION
-]);
